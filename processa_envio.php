da4250e50fa0ae81dcc2682d68ba754a2ebfed28
@RafaelSantos <contatodevrafaelsantos@gmail.com>
2024-12-28 16:08:33 -0300

    Update processa_envio.php

--- processa_envio.php
+++ processa_envio.php
@@ -54,8 +54,8 @@
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'webcompleto2@gmail.com';                     //SMTP username
-        $mail->Password   = '!@#4321';                               //SMTP password
+        $mail->Username   = 'webcompleto23@gmail.com';                     //SMTP username
+        $mail->Password   = '!@#473281';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
